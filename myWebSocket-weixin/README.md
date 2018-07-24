99f903e919f91f267fd72ce2efc73d5e3b244e05
lhp1992 <lihanpeng1992@sina.com>
2018-07-24 15:09:14 +0800

    Signed-off-by: lhp1992 <lihanpeng1992@sina.com>

---
+++ myWebSocket-weixin/README.md
@@ -0,0 +1,112 @@
+# 小程序websocket简易连接及通讯插件
+* author： lhp
+------
+
+> 小程序websocket简易连接及通讯插件。
+
+## 请求与回调
+------
+
+## 参数
+
+#### `开启websocket`
+``` javascript
+const socket = require('../../utils/websocket');
+socket.init('wsurl', options)
+```
+
+#### `wsurl`
+- 您要连接的URL。
+
+#### `options`
+- 选项（见下文）
+
+## Options
+
+#### `reconnectInterval`
+- 尝试重新连接之前延迟的毫秒数。
+- Accepts `integer`
+- Default: `1000`
+
+#### `maxReconnectInterval`
+- 延迟重新连接尝试的最大毫秒数。
+- Accepts `integer`
+- Default: `30000`
+
+####`reconnectDecay`
+- 重新连接延迟的增加率。允许重新连接尝试在问题仍然存在时退出。
+- Accepts `integer` or `float`
+- Default: `1.5`
+
+#### `timeoutInterval`
+- 在关闭和重试之前等待连接成功的最长时间（以毫秒为单位）。
+- Accepts `integer`
+- Default: `2000`
+
+#### `maxReconnectAttempts`
+- 在放弃之前将进行的最大重新连接尝试次数。如果为null，则将继续永久重新连接尝试。
+- Accepts `integer` or `null`.
+- Default: `null`
+
+---
+
+## 方法
+
+``` javascript
+const socket = require('../../utils/websocket');
+```
+
+#### `socket.on(service, callback)`
+- 绑定接口监听
+- service：接口名
+- callback：回调函数（msg.data回调参数）
+- 后台返回格式：
+``` javascript
+{
+	code：错误代码,
+	msg：错误消息,
+	service：推送接口,
+	data：推送内容
+}
+```
+
+#### `socket.emit(service, params)`
+- 向后台发送请求
+- service：接口名
+- params：参数
+- 前台请求格式：
+``` javascript
+{
+	service：接口名,
+	params：参数
+}
+```
+
+#### `socket.off(service, callback)`
+- 移除接口监听
+- service：接口名
+- callback：回调函数
+
+#### `socket.close()`
+- 关闭websocket连接
+
+#### `socket.ifOpen()`
+- websocket是否连接判断
+
+---
+
+## 监听
+
+``` javascript
+socket.onOpen = function(){
+  console.log('连接成功！')
+}
+```
+- 监听WebSocket连接打开事件。
+
+``` javascript
+socket.onClose = function(){
+  console.log('连接关闭！')
+}
+```
+- 监听WebSocket连接关闭事件（断线重连不会触发该回调）。
---
+++ myWebSocket-weixin/websocket.js
@@ -0,0 +1,133 @@
+let socket = {}
+let forcedClose = false
+let isOpen = false
+let message = {}
+
+let init = function (wsip, options){
+  const self = {}
+
+  if (isOpen){
+    return false
+  }
+
+  const settings = {
+    reconnectInterval: 1000,
+    maxReconnectInterval: 30000,
+    reconnectDecay: 1.5,
+    timeoutInterval: 2010,
+    maxReconnectAttempts: null
+  }
+
+  if (!options) { options = {} }
+
+  for (let key in settings) {
+    if (typeof options[key] !== 'undefined') {
+      self[key] = options[key]
+    } else {
+      self[key] = settings[key]
+    }
+  }
+
+  self.reconnectAttempts = 0
+
+  self.open = function(){
+    wx.connectSocket({
+      url: wsip
+    })
+
+    if (self.maxReconnectAttempts && self.reconnectAttempts > self.maxReconnectAttempts) {
+      return;
+    }
+
+    var timeout = setTimeout(function () {
+      socket.close();
+    }, self.timeoutInterval);
+
+    wx.onSocketOpen(function (res) {
+      clearTimeout(timeout);
+      isOpen = true
+      self.reconnectAttempts = 0;
+      socket.onOpen && socket.onOpen()
+    })
+
+    wx.onSocketClose(function (res) {
+      clearTimeout(timeout);
+      isOpen = false
+      if (!forcedClose) {
+        let time = self.reconnectInterval * Math.pow(self.reconnectDecay, self.reconnectAttempts);
+        setTimeout(function () {
+          self.reconnectAttempts++;
+          self.open(true);
+        }, time > self.maxReconnectInterval ? self.maxReconnectInterval : time);
+      } else {
+        socket.onClose && socket.onClose()
+      }
+    })
+
+    wx.onSocketError(function (res) {
+      isOpen = false
+    })
+
+    wx.onSocketMessage(function (res) {
+      let msg
+      try {
+        msg = JSON.parse(res.data);
+      } catch (msg) {
+        return false
+      }
+      // console.log(msg)
+      if (msg.code != 0) {
+        console.error(msg.msg)
+        return false
+      }
+      if (message[msg.service]) {
+        message[msg.service](msg.data)
+      }
+    })
+  }
+  
+  self.open()
+}
+
+socket.init = function (wsip, options){
+  init(wsip, options)
+}
+
+socket.close = function(){
+  forcedClose = true
+  if (isOpen){
+    wx.closeSocket()
+  }
+}
+
+socket.on = function (service, callback) {
+  message[service] = callback
+}
+
+socket.off = function (service, callback) {
+  if (!message[service]) {
+    return false
+  }
+  delete message[service]
+}
+
+socket.emit = function (service, params) {
+  if (!isOpen) {
+    console.error('消息发送失败，未连接到服务器！')
+    return false
+  }
+  var data = {
+    service: service,
+    params: params || {}
+  }
+  // console.log(data)
+  wx.sendSocketMessage({
+    data: JSON.stringify(data)
+  })
+}
+
+socket.ifOpen = function(){
+  return isOpen
+}
+
+module.exports = socket
